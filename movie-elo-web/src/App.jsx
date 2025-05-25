67358d5d31090ea20549926a13273096fc3425ba
jpollnow <147114497+jpollnow@users.noreply.github.com>
2025-05-24 19:31:33 -0500

    Update App.jsx

--- movie-elo-web/src/App.jsx
+++ movie-elo-web/src/App.jsx
@@ -124,8 +124,7 @@ export default function App() {
     formData.append('file', file);
 
     try {
-      const response = await fetch('https://movie-elo-tna6.onrender.com/upload
-', {
+      const response = await fetch('https://movie-elo-tna6.onrender.com/upload', {
         method: 'POST',
         body: formData,
         headers: {
